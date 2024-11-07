03ab4f76c6ba478b67bf8ffb4837dff2ba87b1e1
elijah0kello <elijahokello90@gmail.com>
2024-11-07 17:30:28 +0300

    feat: implement state machine

--- src/index.js
+++ src/index.js
@@ -10,11 +10,66 @@ if(!port){
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const getText = (text)=>{
-    
-}
 
-app.post('/', (req, res) => {
+const UssdMenu = require('ussd-builder');
+let menu = new UssdMenu();
+
+// Define menu states
+menu.startState({
+    run: () => {
+        // use menu.con() to send response without terminating session      
+        menu.con('Welcome. Choose option:' +
+            '\n1. Show Balance' +
+            '\n2. Buy Airtime');
+    },
+    // next object links to next state based on user input
+    next: {
+        '1': 'showBalance',
+        '2': 'buyAirtime'
+    }
+});
+
+menu.state('showBalance', {
+    run: () => {
+        // fetch balance
+        fetchBalance(menu.args.phoneNumber).then((bal)=>{
+            // use menu.end() to send response and terminate session
+            menu.end('Your balance is GHC ' + bal);
+        });
+    }
+});
+
+menu.state('buyAirtime', {
+    run: () => {
+        menu.con('Enter amount:');
+    },
+    next: {
+        // using regex to match user input to next state
+        '*\\d+': 'buyAirtime.amount'
+    }
+});
+
+// nesting states
+menu.state('buyAirtime.amount', {
+    run: () => {
+        // use menu.val to access user input value
+        var amount = Number(menu.val);
+        buyAirtime(menu.args.phoneNumber, amount).then((res)=>{
+            menu.end('Airtime bought successfully.');
+        });
+    }
+});
+
+// Registering USSD handler with Express
+
+app.post('/', (req, res)=>{
+    menu.run(req.body, ussdResult => {
+        res.send(ussdResult);
+    });
+});
+
+
+app.post('/ussd', (req, res) => {
     // Read the variables sent via POST from our API
     const {
         sessionId,
