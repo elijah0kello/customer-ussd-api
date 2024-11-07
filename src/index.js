ba99eb12daebac48259c87f7995ecd2f917fce0e
elijah0kello <elijahokello90@gmail.com>
2024-11-07 16:46:27 +0300

    feat: customer journey

--- src/index.js
+++ src/index.js
@@ -10,6 +10,10 @@ if(!port){
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+const getText = (text)=>{
+    
+}
+
 app.post('/', (req, res) => {
     // Read the variables sent via POST from our API
     const {
@@ -20,6 +24,7 @@ app.post('/', (req, res) => {
     } = req.body;
 
     let response = '';
+
 
     if (text == '') {
         // This is the first request. Note how we start the response with CON
@@ -42,8 +47,29 @@ app.post('/', (req, res) => {
         response = `END Your phone number is ${phoneNumber}`;
     }else if(text == '8') {
         // COMESA Payment
-        
-    } 
+        response = `CON COMESA Payment Services
+        1. Send Money`
+
+    }else if(text == '8*1'){
+        // Send Money
+        response = `CON Enter Account / Phone Number`
+    }else if (text == '8*1*1'){
+        response = `CON Select Country 
+        1. Malawi
+        2. Zambia
+        `;
+    }else if (text == '8*1*1*1'){
+        response = `CON Enter Amount`
+    }else if (text == '8*1*1*1*1'){
+        response = `CON Enter Remarks`
+    }else if (text == '8*1*1*1*1*1'){
+        response = `CON 
+        You are sending Chikondi Banda 300 MWK. 50 ZMW will be debited from your account.
+        1. Proceed
+        2. Abort`
+    }else if (text == '8*1*1*1*1*1*1'){
+        response = `END Transaction in progress`
+    }
     else if ( text == '1*1') {
         // This is a second level response where the user selected 1 in the first instance
         const accountNumber = 'ACC100101';
